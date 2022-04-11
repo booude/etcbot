f6a29816a39f7f38c2663c6a18852198a5f782f8
booude <booude@gmail.com>
2022-04-11 04:01:59 -0300

    prize alert overlay

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from datetime import datetime
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv(os.path.abspath('.env'))
@@ -52,6 +52,7 @@ class Prize(db.Model):
     name = db.Column(db.String(25))
     prize = db.Column(db.String(255))
     date = db.Column(db.Date, default=datetime.utcnow)
+    checkAlert = db.Column(db.Boolean, default=False)
     broadcaster_id = db.Column(db.Integer, db.ForeignKey('broadcaster.id'))
 
 
@@ -60,7 +61,30 @@ headings = ("Name", "Prize", "Date")
 
 @app.route("/", methods=["GET"])
 def table():
-    return render_template("table.html", headings=headings, data=Prize.query.all())
+    prizes = Prize.query.all()
+    return render_template("table.html", headings=headings, prizes=prizes)
+
+
+@app.route("/prizes/overlay", methods=["GET"])
+def overlay():
+    return render_template("overlay.html")
+
+
+@app.route('/api/prizes')
+def api():
+    prizes = Prize.query.order_by(Prize.id.asc()).where(
+        Prize.checkAlert == False).first()
+    try:
+        return jsonify({'id': prizes.id, 'prize': prizes.prize})
+    except:
+        return jsonify(None)
+
+
+@app.route('/update/prizes/<int:id>', methods=['POST'])
+def update(id):
+    Prize.query.filter_by(id=id).update({Prize.checkAlert: True})
+    db.session.commit()
+    return jsonify(success=True)
 
 
 if __name__ == "__main__":
