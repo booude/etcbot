a1b50f276da9ae221e0498fd4f4bdf8df757a530
booude <booude@gmail.com>
2022-04-18 23:24:28 -0300

    function to edit weights of prizes

--- static/python/utils/json.py
+++ static/python/utils/json.py
@@ -12,3 +12,11 @@ def loadprizes(canal):
     with open(os.path.abspath('resources')+f'/{canal}/prizes.json', encoding='utf8') as json_file:
         prizes = json.load(json_file)
         return prizes
+
+
+def editweight(canal, id, weight):
+    with open(os.path.abspath('resources')+f'/{canal}/prizes.json', encoding='utf8') as json_file:
+        prizes = json.load(json_file)
+        prizes[f'{id}']['weight'] = float(weight)
+    with open(os.path.abspath('resources')+f'/{canal}/prizes.json', 'w', encoding='utf-8') as json_file:
+        json.dump(prizes, json_file, indent=2, ensure_ascii=False)
